f2494e36b70e5f3bbc700d130d7dc10576f58420
sovietninja <sovietninjaru@gmail.com>
2025-10-06 19:18:39 +0000

    first

---
+++ main.py
@@ -0,0 +1,22 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+
+def main():
+    print("Hello from aiagent!")
+    
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
+response = client.models.generate_content(
+    model='gemini-2.0-flash-001', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+)
+print(response.text)
+print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}") # type: ignore
+print(f"Response tokens: {response.usage_metadata.candidates_token_count}") # type: ignore
+
+
+
+if __name__ == "__main__":
+    main()
